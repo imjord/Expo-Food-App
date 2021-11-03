26228643d3724947364500391df77bbe4b264a84
imjord <dean.jay.goat@gmail.com>
2021-11-02 19:06:33 -0600

    styled homepage

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+      <View style={styles.homeContainer}>
+        <LinearGradient
+      colors={['#8FBC8F','#DCDCDC','#90EE90']}
+      style={styles.homeContainer}>
+      
+        <View style={styles.title}>
+          
+          <Text style={styles.mainTitle}>Food App</Text>
+          <Text style={styles.subTitle}>Find food</Text>
+          
+        </View> 
+
+
+      </LinearGradient>
+      </View>
+
+
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +33,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  homeContainer: {
+    width: "100%",
+    height: "100%"
+  },
+  title: {
+    marginTop: "30%",
+    alignItems: "center",
+    width: '100%',
+    
+
+  },
+  mainTitle: {
+    fontSize: 40,
+    fontWeight: 'bold',
+  },
+  subTitle: {
+    fontStyle: 'italic'
+  }
 });
